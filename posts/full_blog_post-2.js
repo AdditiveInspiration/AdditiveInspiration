c2830296deac4e025d308596c4774cfb7db05c3f
AdditiveInspiration <11421402@ucdconnect.ie>
2017-07-05 17:00:50 +0100

    a

--- posts/full_blog_post-2.js
+++ posts/full_blog_post-2.js
@@ -2,17 +2,25 @@ document.write("\
 <div class=\"w3-card-4 w3-margin w3-white\" style=\"border: solid 3px #954955;\">\
 <div class=\"w3-row w3-padding w3-dark-gray\" style=\"width:100%\" width = \"200\"  >\
 <div class=\"w3-container\"><p>\
-<h3><b>My Second Print\
+<h3><b>Make your own custom chess set: Scan and Print!\
 </b></h3>\
-<h5>3d Printing <span class=\"w3-opacity\"> 30th June 2017\
+<h5>3d Printing <span class=\"w3-opacity\"> 05th July 2017\
 </span></h5>\
 </div></div>\
 <div class=\"w3-container\">\
 <p>\
-<!TEXT!> Filler Text\
-temp\
-temp\
-temp\
+<!TEXT!> This post is a little bit more advanced: but it should be cheap and accessible to most of the people reading\
+ this blog. We are going to make a customised chess set...where this project gets a little bit more interesting centers around\
+ how we are going to create the models for each chess piece! All you will need is: <p> - A Kinect, the old x-box 360 version with a\
+ usb/power adapter (Version 1)\
+ <p><center><img text-align=\"center\" src=\"./Pictures/base/kinect.jpg\" alt=\"Kinect Sensor\" style=\"width:50%\" style=\" left: 50%\" style=\"top:5%\"></center>\
+ <p> - A 3d Printer <p> - A spinning platform (Office chair works best)<p> - 32 Friends.<p>\
+ <p><center><img text-align=\"center\" src=\"./Pictures/base/scanning.jpg\" alt=\"Scanning a friend\" style=\"width:50%\" style=\" left: 50%\" style=\"top:5%\"></center>\
+<p> So follow along and I promise you won't be disappointed! The final chess pieces are really amazing for the low cost of this project. The following is a Rook, The kind of model you can expect if you follow\
+ this post - only your version could have your granny, mum or even yourself staring back at you!<p>\
+ <p><center><img text-align=\"center\" src=\"./Pictures/base/scan1.jpg\" alt=\"Example Scan\" style=\"width:30%\" style=\" left: 50%\" style=\"top:5%; bottom:50%\">\t<img text-align=\"center\" src=\"./Pictures/base/remi.jpg\" alt=\"Example Scan\" style=\"width:30%\" style=\" left: 50%\" style=\"top:5%; bottom:50%\">\t<img text-align=\"center\" src=\"./Pictures/base/scan2.jpg\" alt=\"Example Scan\" style=\"width:30%\" style=\" left: 50%\" style=\"top:5%\"></center>\
+ <p>\
+\
 \
 hello\
 hello\
