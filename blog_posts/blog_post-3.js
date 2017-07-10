48e31c6c21913c0c944ce050280cb31d1a8c094b
AdditiveInspiration <11421402@ucdconnect.ie>
2017-07-10 13:34:50 +0100

    post 3

---
+++ blog_posts/blog_post-3.js
@@ -0,0 +1,32 @@
+document.write("\
+<div class=\"w3-card-4 w3-margin w3-white\" style=\"border: solid 3px #954955;\">\
+<div class=\"w3-row w3-padding w3-dark-gray\" style=\"width:100%\" width = \"200\"  >\
+<div class=\"w3-container\"><p>\
+<h3><b>My First Print\
+</b></h3>\
+<h5>3d Printing <span class=\"w3-opacity\"> 29th June 2017\
+</span></h5>\
+</div></div>\
+<div class=\"w3-container\">\
+<p>\
+<!TEXT!><p><p>\
+I have always followed new trends in technology closely but none have ever inspired me as much as 3d printing. Making digital objects\
+ physical seems like something from a science fiction movie! As an Engineer, I respect the complexity of 3d printers which represent\
+ the ultimate crossover of mechanical and electronic engineering.<p>\
+ I have bought a new 3d printer with the expectation that I can use it to make quick prototypes for the electronic projects that I do in my spare\
+ time. I have been researching the market for a long time to decide what 3d printer I should buy. Without a lot of money in my budget I have settled on a\
+ DIY 3d printer kit. The kit I chose has a strong community built around it which I am hoping will help when things inevitably go up in smoke..hopefully not literally!. The Tevo Black Widow,\
+ available from AliExpress, has a strong hobbyist community built up around it and appears to be capable..in the right hands..of producing accurate 3d prints.\
+<center><img text-align=\"center\" src=\"./Pictures/base/widow.jpg\" alt=\"Tevo Black Widow\" style=\"width:50%\" style=\" left: 50%\" style=\"top:5%\"></center>\
+<p> When my order arrived it took the guts of a weekend to build. The instructions were relatively unclear but with the aid of the 3d printing community and some\
+ youtube videos the Black Widow was up and running!\
+\
+\
+<div class=\"w3-row\">\
+<div class=\"w3-col m8 s12\">\
+<form method=\"get\" action=\"./posts/post-3.html\">  <p><button class=\"w3-button w3-padding-large w3-white w3-border\"><b>READ MORE</b></button></p></form>\
+</div>\
+<p>\
+</div>\
+</div>\
+</div>");
