329618189223ef415820faf251f2894d60b15c14
AdditiveInspiration <11421402@ucdconnect.ie>
2017-07-10 15:42:16 +0100

    Add files via upload

--- posts/full_blog_post-3.js
+++ posts/full_blog_post-3.js
@@ -2,45 +2,28 @@ document.write("\
 <div class=\"w3-card-4 w3-margin w3-white\" style=\"border: solid 3px #954955;\">\
 <div class=\"w3-row w3-padding w3-dark-gray\" style=\"width:100%\" width = \"200\"  >\
 <div class=\"w3-container\"><p>\
-<h3><b>My First Print\
+<h3><b>Adding a Laser Engraver to a 3d printer\
 </b></h3>\
-<h5>3d Printing <span class=\"w3-opacity\"> 29th June 2017\
+<h5>3d Printing <span class=\"w3-opacity\"> 10th July 2017\
 </span></h5>\
 </div></div>\
 <div class=\"w3-container\">\
 <p>\
-<!TEXT!><p><p>\
-I have always followed new trends in technology closely but none have ever inspired me as much as 3d printing. Making digital objects\
- physical seems like something from a science fiction movie! As an Engineer, I respect the complexity of 3d printers which represent\
- the ultimate crossover of mechanical and electronic engineering.<p>\
- I have bought a new 3d printer with the expectation that I can use it to make quick prototypes for the electronic projects that I do in my spare\
- time. I have been researching the market for a long time to decide what 3d printer I should buy. Without a lot of money in my budget I have settled on a\
- DIY 3d printer kit. The kit I chose has a strong community built around it which I am hoping will help when things inevitably go up in smoke..hopefully not literally!. The Tevo Black Widow,\
- available from AliExpress, has a strong hobbyist community built up around it and appears to be capable..in the right hands..of producing accurate 3d prints.\
-<center><img text-align=\"center\" src=\"./Pictures/base/widow.jpg\" alt=\"Tevo Black Widow\" style=\"width:50%\" style=\" left: 50%\" style=\"top:5%\"></center>\
-<p> When my order arrived it took the guts of a weekend to build. The instructions were relatively unclear but with the aid of the 3d printing community and some\
- youtube videos the Black Widow was up and running!\
+<!TEXT!><p>\
+I have been experimenting with the 3d printer a lot. It is a fantastic tool to make all sorts of prototypes\
+ However, there are still a lot of limitations with printers from every price range. The more practice I get, the more and more consistant\
+ results I achieve. I have been looking into ways to upgrade the printer to increase functionality for a long time and I have decided to do one\
+ upgrade in particular: Add a laser engraver.<p>\
+ I have been trying to make custom board games recently..having designed some settlers of catan models I am getting tired of the time it takes\
+ to do even small prints! With this in mind I have decided to write a guide on adding a laser engraver to any 3d printer. It is actually quite simple..\
+ 3d printers are composed of a hot end, extruder and a way to move a carriage along the x, y and z axis. Simply adding a laser module to the moving carriage and\
+ fixing the z axis (height) to the focal length of the laser will allow you to etch most materials with computer precision. THIS POST IS A LITTLE MORE DANGEROUS SO DO NOT TRY THIS IF YOU\
+ DONT KNOW WHAT YOU ARE DOING. I TAKE NO RESPONSIBILITY FOR ANY EYE INJURIES\
+  <p><center><img text-align=\"center\" src=\"./Pictures/base/b3p1.jpg\" alt=\"Scanning a friend\" style=\"width:50%; max-width:400px;\"  style=\" left: 50%\" style=\"top:5%\"></center><p>\
 \
-<p>To say I was excited would be an understatement..which leads to my first piece of advice. DO NOT RUSH INTO YOUR FIRST 3d PRINT.\
- Even when the printer powers on, make sure everything is working properly before starting any 3d print. When I switched my printer on it had every appearance of working properly\
- but I had connected the y motor the wrong way around and nearly had everything crash down around me when I tried to autohome for the first time and accidentally sent the extruder\
- to y-infinity!!\
 <p>\
-<p>This blog/website is really going to be a slow burner...like all things..especially 3d printing.. It is going to take time! <p>\
-  My first suggestion is to callibrate your printer... big or small, cheap or expensive...IT WILL NEED CALLIBRATION. There is really no need to skip this step or your impatience will cost you a lot of time.\
-<p> Step 1.Callibrate your E Steps, a relatively simple step to ensure the extruder pushes out the correct amount of material. I will not give any tutorial on these because they can so easily be googled!!\
-<p> Step 2. Level your printer bed properly..all you need is a sheet of paper and some patience. Google how to do this..It's very simple but will have the greatest effect on the results you get\
-<p> Step 3. Print some callibration files. Grab a 20 mm callibration cube from thingiverse. Load it onto your printer and measure the results.. This print will help you determine if the x,y and z motors are\
- callibrated properly. If the sides dont measure 20 mm you can adjust your motor steps to ensure it will print properly the next time. Each side will correspond to a motor axis.\
-<p><center><img text-align=\"center\" src=\"./Pictures/base/c_cube.jpg\" alt=\"Calibration Cube\" style=\"width:50%\" style=\" left: 50%\" style=\"top:5%\"></center>\
- <p>  Vertical edge = Z axis\
-  Left-Right Edge = X axis\
-  Front-Back Edge = Y axis\
-<p>\
-Adjust the motor steps by the following exquation: ((Actual Measured Edge - 20 mm) / 20 mm ) by the current motor e-steps setting.\
-<p> The quality of each side of the cube will also give you an idea of how well the printer is working. It can help to visually describe problems with your printer while asking for help on forums etc.\
- I hope you have liked the first post, please comment below and keep following to help this website grow and develop!\
 \
+\\
 \
 \
 <div class=\"w3-row\">\
