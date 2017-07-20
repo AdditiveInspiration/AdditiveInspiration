16c6fd954a72a7709cb41591f93e5c9450d18943
AdditiveInspiration <11421402@ucdconnect.ie>
2017-07-20 09:15:18 +0100

    Add files via upload

---
+++ blog_posts/blog_post-4.js
@@ -0,0 +1,30 @@
+document.write("\
+<div class=\"w3-card-4 w3-margin w3-white\" style=\"border: solid 3px #954955;\">\
+<div class=\"w3-row w3-padding w3-dark-gray\" style=\"width:100%\" width = \"200\"  >\
+<div class=\"w3-container\"><p>\
+<h3><b>3d Printing: Where to start? Part 1\
+</b></h3>\
+<h5>3d Printing <span class=\"w3-opacity\"> 17th July 2017\
+</span></h5>\
+</div></div>\
+<div class=\"w3-container\">\
+<p>\
+<!TEXT!><p><p>\
+  I have decided to write a beginners guide to 3d printing. I think it is a common trend that people\
+  will buy a 3d printer with great intentions only to abandon it when things start to go wrong! There have been so many times\
+  that I have wanted to throw my printer out a window! Failed prints become less frequent with experience but I think a simple guide could help to reduce the learning curve.<p>\
+  With that being said, from my experience, I think there is a lot of advice and support available which can help overcome nearly every issue with\
+  3d printing that any beginner might encounter. Taking time to calibrate every 3d printer can make a huge difference in the quality of results. I will try to make this guide general so it can be applicable\
+  to as many 3d printers as possible.\
+  <p><center><img text-align=\"center\" src=\"./Pictures/base/b4p1.jpg\" alt=\"3d printing problems\" style=\"width:50%; max-width:400px;\"  style=\" left: 50%\" style=\"top:5%\"></center>\
+  This will cover:<p>FILAMENT, CALIBRATION, TEMPERATURE, SIMPLE PRINTS<p>\
+  \
+\
+<div class=\"w3-row\">\
+<div class=\"w3-col m8 s12\">\
+<form method=\"get\" action=\"./posts/post-4.html\">  <p><button class=\"w3-button w3-padding-large w3-white w3-border\"><b>READ MORE</b></button></p></form>\
+</div>\
+<p>\
+</div>\
+</div>\
+</div>");
---
+++ blog_posts/blog_post-5.js
@@ -0,0 +1,30 @@
+document.write("\
+<div class=\"w3-card-4 w3-margin w3-white\" style=\"border: solid 3px #954955;\">\
+<div class=\"w3-row w3-padding w3-dark-gray\" style=\"width:100%\" width = \"200\"  >\
+<div class=\"w3-container\"><p>\
+<h3><b>3d Printing: Where to start? Part 2\
+</b></h3>\
+<h5>3d Printing <span class=\"w3-opacity\"> 18th July 2017\
+</span></h5>\
+</div></div>\
+<div class=\"w3-container\">\
+<p>\
+<!TEXT!><p><p>\
+  This is the second part in a series designed to ge tbeginners up and running in the\
+  world of 3d-printing. In the last section I went through some calibration techniques, filament types and temperature settings.\
+  I ended with some links to stl files which are simple and easy to print for beginners. To continue on I think it would be useful to run through some of the free software available to create printable models!\
+  <p><center><img text-align=\"center\" src=\"./Pictures/base/b5p1.jpg\" alt=\"3d printing software\" style=\"width:50%; max-width:400px;\"  style=\" left: 50%\" style=\"top:5%\"></center>\
+  This will cover:<p>FREE SOFTWARE, CREATING GCODE, LEVELING THE BED, CUSTOM DESIGN<p>\
+  \
+  \
+  \
+  \
+\
+<div class=\"w3-row\">\
+<div class=\"w3-col m8 s12\">\
+<form method=\"get\" action=\"./posts/post-5.html\">  <p><button class=\"w3-button w3-padding-large w3-white w3-border\"><b>READ MORE</b></button></p></form>\
+</div>\
+<p>\
+</div>\
+</div>\
+</div>");
